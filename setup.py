ba6283f599324574a5c33fb816cf935ec01ed79b
DeniroNl <denibutfuture@gmail.com>
2026-02-16 20:58:19 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,17 +3,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='course_lib', # Название вашей программы
-    version='0.0.1', # Версия вашей программы.
+    name='course_lib',
+    version='0.0.1',
     packages=find_packages("."),
-    scripts=["bin/task_course.py"], # Расположение главного исполняемого файла.
-    url='https://github.com/...', # Адрес репозитория с вашей курсовой работой.
+    scripts=["bin/task_course.py"],
+    url='https://github.com/DeniroNl/Expenditure-and-revenue-accounting',
     license='Apache-2.0',
-    author='Бут Денис Олегович', # ФИО автора.
-    author_email='denibutfuture@gmail.com', # Электронная почта автора.
-    description='...', # Описание вашей поделки. Что она может, для чего сделана.
+    author='Бут Денис Олегович',
+    author_email='denibutfuture@gmail.com',
+    description='Пакет для ведения учёта расходов и доходов',
     include_package_data=True,
-    install_requires=[
-      # Список зависимостей если есть.
-      ],
+    install_requires=["datetime"],
 )
